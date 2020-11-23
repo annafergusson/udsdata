3fc0a117abe3192860b6f59f897a9f8f0153c31d
Anna Fergusson <annafergussonnz@gmail.com>
2020-11-23 21:22:08 +1300

    Compressed data files

--- data-raw/babynames.R
+++ data-raw/babynames.R
@@ -2,13 +2,13 @@ library(tidyverse)
 library(lubridate)
 
 url <- "https://www.ssa.gov/oact/babynames/names.zip"
-download.file(url, dest="data-raw/dataset.zip", mode="wb")
-unzip("data-raw/dataset.zip",  exdir="data-raw")
+download.file(url, dest="data-raw/temp/dataset.zip", mode="wb")
+unzip("data-raw/temp/dataset.zip",  exdir="data-raw/temp")
 keywords <- "yob"
-files <- list.files("data-raw", pattern = keywords)
+files <- list.files("data-raw/temp", pattern = keywords)
 
 us_data <- map_df(1 : length(files), function(i){
-  this_data <- read_csv(paste0("data-raw/",files[i]),
+  this_data <- read_csv(paste0("data-raw/temp/",files[i]),
                         col_names = FALSE,
                         col_types = cols(col_character(),
                                          col_character(),
@@ -21,6 +21,8 @@ us_data <- map_df(1 : length(files), function(i){
   this_data
 })
 
+unlink("data-raw/temp/*")
+
 # get baby name data
 url <- "https://catalogue.data.govt.nz/dataset/01ee87cd-ecf8-44a1-ad33-b376a689e597/resource/0b0b326c-d720-480f-8f86-bf2d221c7d3f/download/baby-names-2020-1-6.csv"
 nz_data <- read_csv(url, col_types = cols(Year = col_integer(),
@@ -31,10 +33,11 @@ nz_data <- read_csv(url, col_types = cols(Year = col_integer(),
 
 babynames <- bind_rows(us_data, nz_data)
 
-usethis::use_data(babynames)
+usethis::use_data(babynames, compress = "xz", overwrite = T)
 
 babyjustnames <- babynames %>%
   group_by(Name, Year, Country) %>%
   summarise(Count = sum(Count))
 
-usethis::use_data(babyjustnames)
+usethis::use_data(babyjustnames, compress = "xz", overwrite = T)
+
